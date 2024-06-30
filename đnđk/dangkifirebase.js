e4f8f26f2bd7bd3ab12750bf3fe6b92bf440d790
hainam <155862587+hainamtft@users.noreply.github.com>
2024-06-30 19:24:25 +0700

    123

--- đnđk/dangkifirebase.js
+++ đnđk/dangkifirebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
-
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCRWIVlXQnEwuBR67ggUulOZCMPYThZ3nY",
@@ -11,21 +10,26 @@ const firebaseConfig = {
     messagingSenderId: "1023573683053",
     appId: "1:1023573683053:web:229a2a9495b4fb25064864",
     measurementId: "G-5DGX9B37FM"
-  };
+};
 
 const app = initializeApp(firebaseConfig);
-
-
 const db = getDatabase(app);
-// document.getElementById("button").addEventListener('click', function(e){
-//     e.preventDefault();
-//     set(ref(db, 'user/' + document.getElementById("username").value),
-//     {
 
-//       username: document.getElementById("username").value,
-//       gmail: document.getElementById("gmail").value,
-//       phonenumber : document.getElementById("sdt").value
+document.getElementById("button").addEventListener('click', function(e) {
+    e.preventDefault();
+    const username = document.getElementById("username").value;
+    const gmail = document.getElementById("gmail").value;
+    const phonenumber = document.getElementById("sdt").value;
 
-//     });
-
-//    })
+    set(ref(db, 'user/' + username), {
+        username: username,
+        gmail: gmail,
+        phonenumber: phonenumber
+    })
+    .then(() => {
+        alert("Đăng ký thành công!");
+    })
+    .catch((error) => {
+        alert("Lỗi: " + error.message);
+    });
+});
